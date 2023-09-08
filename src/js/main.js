976129745b76c3283853a7f7e5b685fb079b1d09
Voron Porto <voronenkotg@gmail.com>
2023-09-08 13:56:57 +0300

    basic structure created

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
 // import modals from './modules/sampleDir';
 import '../sass/style.scss';
 
-import select from "./modules/select";
-
-
 
 window.addEventListener('DOMContentLoaded', () =>{
     "use strict";

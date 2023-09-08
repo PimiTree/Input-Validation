9024ccb6a8a02c0b66741a83d0f45688e5b525d7
Voron Porto <voronenkotg@gmail.com>
2023-09-08 13:43:09 +0300

    create template for Voron Form Validations service

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// import modals from './modules/sampleDir';
+import '../sass/style.scss';
+
+import select from "./modules/select";
+
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+    "use strict";
+
+
+});
---
+++ vite.config.js
@@ -0,0 +1,106 @@
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { defineConfig } from "vite";
+
+export default defineConfig(() => {
+    return {
+      base: "",
+      root: './src',
+      build: {
+        outDir: '../dist',
+        assetsDir: '',
+        assetsInlineLimit: 0,
+        rollupOptions: {
+          output: {
+            entryFileNames: 'js/[name]-[hash].js',
+            chunkFileNames: 'js/[name]-[hash].js',
+            assetFileNames: assetInfo => {
+              const info = assetInfo.name.split('.');
+              const extType = info[info.length - 1];
+              if (/\.(png|jpe?g|gif|svg|webp|webm|mp3)$/.test(assetInfo.name)) {
+                return `img/[name]-[hash].${extType}`;
+              }
+              if (/\.(css)$/.test(assetInfo.name)) {
+                return `css/[name]-[hash].${extType}`;
+              }
+              if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
+                return `fonts/[name]-[hash].${extType}`;
+              }
+              return `[name]-[hash].${extType}`;
+            },
+          },
+        },
+      },
+      plugins: [
+        ViteImageOptimizer({
+            DEFAULT_OPTIONS: {
+                test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+                exclude: undefined,
+                include: undefined,
+                includePublic: true,
+                logStats: true,
+                ansiColors: true,
+                svg: {
+                  multipass: true,
+                  plugins: [
+                    {
+                      name: 'preset-default',
+                      params: {
+                        overrides: {
+                          cleanupNumericValues: false,
+                          removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+                        },
+                        cleanupIDs: {
+                          minify: false,
+                          remove: false,
+                        },
+                        convertPathData: false,
+                      },
+                    },
+                    'sortAttrs',
+                    {
+                      name: 'addAttributesToSVGElement',
+                      params: {
+                        attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+                      },
+                    },
+                  ],
+                },
+                png: {
+                  // https://sharp.pixelplumbing.com/api-output#png
+                  quality: 100,
+                },
+                jpeg: {
+                  // https://sharp.pixelplumbing.com/api-output#jpeg
+                  quality: 100,
+                },
+                jpg: {
+                  // https://sharp.pixelplumbing.com/api-output#jpeg
+                  quality: 100,
+                },
+                tiff: {
+                  // https://sharp.pixelplumbing.com/api-output#tiff
+                  quality: 100,
+                },
+                // gif does not support lossless compression
+                // https://sharp.pixelplumbing.com/api-output#gif
+                gif: {},
+                webp: {
+                  // https://sharp.pixelplumbing.com/api-output#webp
+                  lossless: true,
+                },
+                avif: {
+                  // https://sharp.pixelplumbing.com/api-output#avif
+                  lossless: true,
+                },
+              }
+        }),
+       
+    ],
+    
+   
+    }
+})
+
+
+
+
